e864aa461e1de34b6609ceeb86f752fc082fad11
Karen Boerner <kboerner@ptc.com>
2024-06-29 13:44:32 +0200

    adding the landing page

---
+++ src/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+function Navigation() {
+return (
+<ul>
+<li><Link to="/">Home</Link></li>
+</ul>
+);
+}
+export default Navigation;
