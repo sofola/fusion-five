18bf4369b9533730ecc14dc28cd72779caec53ea
Karen Boerner <kboerner@ptc.com>
2024-06-29 22:07:45 +0200

    adds starting page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,31 +8,31 @@ const Menu = () => {
             {/* View 1 */}
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
                 <img src={Banner} alt="Banner 1" style={{ width: '100%', maxWidth: '500px' }} />
-                <div class="container">
-                    <button class="menuBtn">View All Our Products</button>
+                <div className="container">
+                    <button className="menuBtn">View All Our Products</button>
                 </div>
             </div>
 
             {/* View 2 */}
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
                 <img src={Banner} alt="Banner 2" style={{ width: '100%', maxWidth: '500px' }} />
-                <div class="container">
-                    <button class="menuBtn"><a href='https://www.nivea.co.uk/advice/findmyroutine#/'>Analyze Your Skin</a></button>
+                <div className="container">
+                    <button className="menuBtn"><a href='https://www.nivea.co.uk/advice/findmyroutine#/'>Analyze Your Skin</a></button>
                 </div>
             </div>
 
             {/* View 3 */}
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
                 <img src={Banner} alt="Banner 3" style={{ width: '100%', maxWidth: '500px' }} />
-                <div class="container">
-                <button class="menuBtn">Discover Your Perfect Skincare Routine</button>
+                <div className="container">
+                    <button className="menuBtn"><a href='/question'>Discover Your Perfect Skincare Routine</a></button>
                 </div>      </div>
 
             {/* View 4 */}
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
                 <img src={Banner} alt="Banner 4" style={{ width: '100%', maxWidth: '500px' }} />
-                <div class="container">
-                    <button class="menuBtn">Daily Skincare Challenge</button>
+                <div className="container">
+                    <button className="menuBtn">Daily Skincare Challenge</button>
                 </div>      </div>
 
         </div>
