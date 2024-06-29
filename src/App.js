2a7b1c981c310df047f2831f3cbf3b5a03839054
Karen Boerner <kboerner@ptc.com>
2024-06-29 19:53:49 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
+import Menu from './components/Menu';
+import ChallengeExplenation from "./components/ChallengeExplenation";
+import Start from "./components/Start"
+import Introduction from './components/Introduction'
+import GenderSelection from './components/GenderSelection'
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import Menu from './components/Menu';
-import Introduction from './components/Introduction'
-import GenderSelection from './components/GenderSelection'
 
 const App = () => {
   return (
     <Router>
       <div>
         <Switch>
+          <Route path="/" exact component={Start} />
           <Route path="/menu" exact component={Menu} />
           <Route path="/gender-selection" component={GenderSelection} />
           <Route path="/question" exact component={Introduction} />
+          <Route path="/challenge_explenation" exact component={ChallengeExplenation} />
         </Switch>
       </div>
     </Router>
