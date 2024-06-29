0a8ae0f5c0689465f4fd21cf57b4df48bdf67885
Sophie <sophie.folawiyo@dfki.de>
2024-06-29 19:37:38 +0200

    ml integration

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from product_suggestion import suggest_products
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,8 @@ def submit():
     print('Request received')
     data = request.json
     print('Received data:', data)
+    if 'My skin type is' in data.keys():
+        suggest_products(data['My skin type is'])
     if data is None:
         return jsonify({'status': 'error', 'message': 'No data received'}), 400
     return jsonify({'status': 'success', 'data': data})
