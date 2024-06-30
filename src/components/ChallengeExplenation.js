4a51bf508e4e9fb339add8cd10bd1f076bf95d8e
Chihoko Neudeck <61164812+chihoko12@users.noreply.github.com>
2024-06-30 04:06:50 +0200

    fix: recover ChallengeExplanation files

---
+++ src/components/ChallengeExplenation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/ChallengeExplenation.css'
+
+const ChallengeExplenation = () => {
+  return (
+    <div>
+      <p class="explantion_text">Brief explanation about this challenge</p>
+      <div class="div_prompts">
+
+      </div>
+    </div>
+  );
+}
+
+export default ChallengeExplenation
